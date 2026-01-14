e490ac7dd5d93e9b1cb3cb0c568e7d7445263ab9
Harshvardhan jadhav <jadhavharsh6704@gmail.com>
2026-01-14 23:47:56 +0530

    Add some change

--- models/user.js
+++ models/user.js
@@ -4,10 +4,13 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     }
 });
 
-userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose, {
+    usernameField: 'email'
+});
 
 module.exports = mongoose.model("User", userSchema);
