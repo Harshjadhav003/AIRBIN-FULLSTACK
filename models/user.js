5292fd42f1b053316bbe5cf86b0fe4cac0794c9c
Harshvardhan jadhav <jadhavharsh6704@gmail.com>
2026-02-10 13:42:55 +0530

    Fix user Schema

--- models/user.js
+++ models/user.js
@@ -2,18 +2,18 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    username: {
-        type: String //  define it so it’s not null
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String // required internally by passport-local-mongoose
+  }
 });
 
 userSchema.plugin(passportLocalMongoose, {
-    usernameField: 'email'
+  usernameField: "email"
 });
 
 module.exports = mongoose.model("User", userSchema);
