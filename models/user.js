72faafe00e6a74c28288994899442799daea1518
Harshvardhan jadhav <jadhavharsh6704@gmail.com>
2026-02-10 13:27:57 +0530

    Fix user Schema

--- models/user.js
+++ models/user.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
-const passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         unique: true
+    },
+    username: {
+        type: String //  define it so it’s not null
     }
 });
 
